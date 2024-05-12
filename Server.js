1c5a6dc51b81f2f42b8599e6b11a371bae6ee7d0
FatemaKotb <Fatema.Kotb02@eng-st.cu.edu.eg>
2024-05-13 00:58:59 +0300

    Updated the Class and Server from an old repo.

--- Server.js
+++ Server.js
@@ -1,107 +1,84 @@
-import express from "express";
-import http from "http";
-import { Server } from "socket.io";
+const express = require("express");
+const http = require("http");
 
-import jwt from "jsonwebtoken";
+const Server = require("socket.io").Server;
+const CRDT = require("./CRDT").CRDT;
 
-import { User } from "../db/models/User.js";
 
 // Application and servers initialization
 const app = express();
 
 const server = http.createServer(app);
 
+const port = 5000;
+
+server.listen(port, () => {
+    console.log("Server is Up on port " + port);
+});
+
 const io = new Server(server, {
     path: "/socket.io",
     cors: {
-        origin: "*",
-        methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+        origin: "*"
     },
 });
 
-// Helper function
-export const getReceiverSocketId = (receiverId) => {
-    return userSocketMap[receiverId];
-};
 
-// This object maps user IDs to socket IDs.
-const userSocketMap = {}; 
-
+// This map stores a CRDT instance for each document.
+const documentCRDTs = new Map();
 
 io.on("connection", async (socket) => {
-    
     console.log(`A new user is connect with socked ID: ${socket.id}`);
 
-    ///////////////////////////////////////////////////////////////////////////////////////////////////////
-    // Get the user from the provided token to fill the userSocketMap.
-
-    // Extract the token from the query parameter.
-    let token;
-     
-    if (socket.handshake.query.token) {
-        token = socket.handshake.query.token.split(" ")[1];
-        console.log(`The provided token: ${token}`)
+    // Extract the username and document ID from the query parameter.
+    let username, docId;
+    if (socket.handshake.query.username && socket.handshake.query.docId) {
+        username = socket.handshake.query.username;
+        docId = socket.handshake.query.docId;
+        console.log(`The provided username: ${username}`)
+        console.log(`The provided document ID: ${docId}`)
     } else {
-        console.log("Token is not provided");
+        console.log("Username or document ID is not provided");
+        socket.disconnect(true);
     }
 
-    let user_token;
+    // Join the document room
+    socket.join(docId);
 
-    try {
-        user_token = jwt.verify(token, process.env.JWT_SECRET);
-    } catch (err) {
-        console.log({
-            err: { status: 401, message: `Invalid Token: ${err.message}` },
-        });
+    // If there's no CRDT for this document yet, create one.
+    if (!documentCRDTs.has(docId)) {
+        documentCRDTs.set(docId, new CRDT());
+        // After conncting to the db, handling this condition should be retrieving the CRDT from the database.
     }
 
-    // Get the user id from the token.
-    const user_id = user_token._id;
-
-    // Get the user from the id.
-    const user = await User.findById(user_id);
-
-    if (!user) {
-        console.log({ err: { status: 404, message: "User not found" } });
-    } else {
-        userSocketMap[user_id] = socket.id;
-    }
+    // Get the CRDT for this document.
+    const crdt = documentCRDTs.get(docId);
 
     // Send the CRDT from the server to the client
     socket.emit('crdt', crdt);
 
     // Listen for insert and delete events
     socket.on('insert', (node) => {
-        crdt.insert(node);
-        socket.to(room).emit('insert', node); // broadcast the insert event to all other clients in the room
+        crdt.insertPosition(node);
+        documentCRDTs.set(docId, crdt);
+        socket.to(docId).emit('insert', node); // broadcast the insert event to all other clients in the room
     });
 
     socket.on('delete', (node) => {
-        crdt.delete(node);
-        socket.to(room).emit('delete', node); // broadcast the delete event to all other clients in the room
+        crdt.deletePosition(node);
+        documentCRDTs.set(docId, crdt);
+        socket.to(docId).emit('delete', node); // broadcast the delete event to all other clients in the room
     });
 
     socket.on("disconnect", () => {
         console.log("user disconnected", socket.id);
-        delete userSocketMap[user_id];
 
         // Check if this is the last user in the room, if so call cleanup function from CRDT
-        if (isLastUserInRoom(user_id)) {
+        if (isLastUserInRoom(username)) {
             crdt.cleanup();
         }
     });
-
-    // socket.on() is used to listen to the events. can be used both on client and server side
-    socket.on("disconnect", () => {
-        console.log("user disconnected", socket.id);
-        delete userSocketMap[user_id];
-    });
 });
-
-// Set up event listeners:
-// on connection send the CRDT from the server to the client
-// on insert or delete call the corresponsing function in the CRDT class
-// on disconnect delete the user from the userSocketMap & check if this is the last user in the room, if so you should call clean up function from crdt
 
 export { app, io, server };
 
